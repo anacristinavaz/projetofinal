648b444121d52187b8f55cbe26bf9a0b2465d9ce
arthurfrederico <arthurfrederico.mello@gmail.com>
2023-03-13 15:47:07 -0300

    Primeiro commit arthur

--- src/main/java/br/com/melhorgrupo/projetofinal/repo/ClienteRepo.java
+++ src/main/java/br/com/melhorgrupo/projetofinal/repo/ClienteRepo.java
@@ -1,5 +1,9 @@
 package br.com.melhorgrupo.projetofinal.repo;
 
-public interface ClienteRepo {
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.melhorgrupo.projetofinal.model.Cliente;
+
+public interface ClienteRepo extends CrudRepository<Cliente,Integer> {
 
 }
