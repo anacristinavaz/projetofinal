e9efefadc60328bafaddd864767015a00ac7eca2
motoblur.connect@gmail.com <motoblur.connect@gmail.com>
2023-03-13 13:28:21 -0300

    DiogoModelCliente

--- src/main/java/br/com/melhorgrupo/projetofinal/model/Cliente.java
+++ src/main/java/br/com/melhorgrupo/projetofinal/model/Cliente.java
@@ -1,5 +1,55 @@
 package br.com.melhorgrupo.projetofinal.model;
 
+import jakarta.persistence.*;
+
+@Entity(name = "tbl_cliente")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Long id_cliente;
+    @Column(name="nome_cliente")
+    private String nome_cliente;
+
+    public Long getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(Long id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNome_cliente() {
+        return nome_cliente;
+    }
+
+    public void setNome_cliente(String nome_cliente) {
+        this.nome_cliente = nome_cliente;
+    }
+
+    public String getCpf_cliente() {
+        return cpf_cliente;
+    }
+
+    public void setCpf_cliente(String cpf_cliente) {
+        this.cpf_cliente = cpf_cliente;
+    }
+
+    public String getTelefone_cliente() {
+        return telefone_cliente;
+    }
+
+    public void setTelefone_cliente(String telefone_cliente) {
+        this.telefone_cliente = telefone_cliente;
+    }
+
+    @Column(name="cpf_cliente")
+    private String cpf_cliente;
+    @Column(name="telefone_cliente")
+    private String telefone_cliente;
+
+
+
 
 }
