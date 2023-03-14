d648d0b60c8c31fd9d4708a2e41eb4c9ca2035d6
motoblur.connect@gmail.com <motoblur.connect@gmail.com>
2023-03-14 16:05:10 -0300

    DiogoClienteControle

--- src/main/java/br/com/melhorgrupo/projetofinal/controller/ClienteController.java
+++ src/main/java/br/com/melhorgrupo/projetofinal/controller/ClienteController.java
@@ -1,5 +1,39 @@
 package br.com.melhorgrupo.projetofinal.controller;
 
+import br.com.melhorgrupo.projetofinal.model.Cliente;
+import br.com.melhorgrupo.projetofinal.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente c) {
+        Cliente novoCliente = clienteService.cadastrarCliente(c);
+        return new ResponseEntity<>(novoCliente, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<ArrayList<Cliente>> listarClientes() {
+        ArrayList<Cliente> c = clienteService.recuperarTodos();
+        return new ResponseEntity<>(c, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarCliente(@PathVariable("id") Long idCliente) {
+        Cliente c = clienteService.recuperarPeloID(Math.toIntExact(idCliente));
+        if (c == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(c, HttpStatus.OK);
+    }
+
 
 }
