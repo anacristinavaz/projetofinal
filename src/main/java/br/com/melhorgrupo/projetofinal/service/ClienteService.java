81c981bcdac307c6e752c7b65ebfb75d3ea57bee
motoblur.connect@gmail.com <motoblur.connect@gmail.com>
2023-03-14 09:55:24 -0300

    DiogoServicos

--- src/main/java/br/com/melhorgrupo/projetofinal/service/ClienteService.java
+++ src/main/java/br/com/melhorgrupo/projetofinal/service/ClienteService.java
@@ -1,5 +1,14 @@
 package br.com.melhorgrupo.projetofinal.service;
 
+import br.com.melhorgrupo.projetofinal.model.Cliente;
+
 public interface ClienteService {
+
+    public Cliente cadastrarCliente(Cliente c);
+
+    public Iterable<Cliente> recuperarTodos();
+
+    public Cliente recuperarPeloID(int idCliente);
+
 
 }
