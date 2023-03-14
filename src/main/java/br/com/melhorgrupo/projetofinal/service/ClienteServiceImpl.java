58d724ea6159d403808f67e28330ac009582e383
motoblur.connect@gmail.com <motoblur.connect@gmail.com>
2023-03-14 12:54:10 -0300

    ServiceAtualização

--- src/main/java/br/com/melhorgrupo/projetofinal/service/ClienteServiceImpl.java
+++ src/main/java/br/com/melhorgrupo/projetofinal/service/ClienteServiceImpl.java
@@ -1,13 +1,16 @@
 package br.com.melhorgrupo.projetofinal.service;
 
 import br.com.melhorgrupo.projetofinal.model.Cliente;
+import br.com.melhorgrupo.projetofinal.model.Movimentacao;
 import br.com.melhorgrupo.projetofinal.repo.ClienteRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 
 @Component
-public class ClienteServiceImpl {
+public class ClienteServiceImpl implements ClienteService {
 
     @Autowired
     private ClienteRepo Repo;
@@ -16,14 +19,18 @@ public class ClienteServiceImpl {
         return Repo.save(c);
     }
 
-    public Iterable<Cliente> recuperarTodos() {
-        return Repo.findAll();
+    @Override
+    public ArrayList<Cliente> recuperarTodos(int Cliente) {
+        return null;
+    }
+
+    public ArrayList<Cliente> recuperarTodos() {
+        return (ArrayList<Cliente>) Repo.findAll();
     }
 
     public Cliente recuperarPeloID(int id) {
         return Repo.findById(id).orElse(null);
     }
-
 
 
 }
