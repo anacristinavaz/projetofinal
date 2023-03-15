f32d2701ebd48ec106994ae14fbee7ea124d953e
anacristinavaz <anacrisvaz@hotmail.com>
2023-03-15 15:39:39 -0300

    resolvendo

--- src/main/java/br/com/melhorgrupo/projetofinal/controller/ContaController.java
+++ src/main/java/br/com/melhorgrupo/projetofinal/controller/ContaController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.melhorgrupo.projetofinal.model.Cliente;
 import br.com.melhorgrupo.projetofinal.model.Conta;
 import br.com.melhorgrupo.projetofinal.service.ContaServiceImpl;
 
