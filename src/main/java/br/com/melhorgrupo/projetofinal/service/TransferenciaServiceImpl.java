a021860199cf91b0ffbafb56f552f2914d3e55f4
anacristinavaz <anacrisvaz@hotmail.com>
2023-03-16 11:55:08 -0300

    AnaAlteraTransf

--- src/main/java/br/com/melhorgrupo/projetofinal/service/TransferenciaServiceImpl.java
+++ src/main/java/br/com/melhorgrupo/projetofinal/service/TransferenciaServiceImpl.java
@@ -3,12 +3,9 @@ package br.com.melhorgrupo.projetofinal.service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TransferenciaServiceImpl implements TransferenciaService{
-	@Autowired
-	private MovimentacaoService serviceMovi;
 	
 	@Override
 	public boolean transferirValores(int contaOrigem, int contaDestino, double valor) {
-		serviceMovi(contaOrigem);
 		return false;
 	}
 
