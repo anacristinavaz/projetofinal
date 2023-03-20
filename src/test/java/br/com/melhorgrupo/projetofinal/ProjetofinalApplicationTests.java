db9045723c75af8538a561f9480eeb7510d16a83
anacristinavaz <anacrisvaz@hotmail.com>
2023-03-20 12:36:59 -0300

    AnaRailway

--- src/test/java/br/com/melhorgrupo/projetofinal/ProjetofinalApplicationTests.java
+++ src/test/java/br/com/melhorgrupo/projetofinal/ProjetofinalApplicationTests.java
@@ -1,13 +1,24 @@
 package br.com.melhorgrupo.projetofinal;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.com.melhorgrupo.projetofinal.service.TransferenciaService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjetofinalApplicationTests {
-
+	
+	@Autowired
+	private TransferenciaService trans;
+	
 	@Test
-	void contextLoads() {
+	public void transferenciaTeste() {
+		boolean teste = trans.transferirValores(1, 3, 2000);
+		assertTrue(teste);
 	}
-
 }
